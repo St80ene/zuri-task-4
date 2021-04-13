5d994f1f43fd6a4459bf6cf91b757fda1bbf2b3c
@st80ene <etienejames5@gmail.com>
2021-04-13 22:43:13 +0100

    Refactored my code to follow best practices

--- index.js
+++ index.js
@@ -5,16 +5,13 @@ async function posts() {
 	const url = 'http://jsonplaceholder.typicode.com/posts';
 	let response = await axios.get(url);
 	let data = JSON.stringify(response.data, null, 2);
-
-	return data;
-}
-
-posts().then(result => {
-	fs.writeFile('./result/posts.json', result, (error) => {
+	fs.writeFileSync('./result/posts.json', data, (error) => {
 		if (error) {
-			console.error(error);
-			return
+			return error;
 		}
 		return 'File created...';
 	});
-}).catch(error => console.error(error))
+}
+
+posts();
+
